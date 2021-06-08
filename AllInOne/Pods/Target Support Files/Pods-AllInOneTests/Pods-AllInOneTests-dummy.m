7b887173d781c00d9013579bda0054ab1db1b53e
hayasilin <harvey.hayasi@gmail.com>
2021-06-08 14:18:24 +0800

    remove unneed files

--- AllInOne/Pods/Target Support Files/Pods-AllInOneTests/Pods-AllInOneTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AllInOneTests : NSObject
-@end
-@implementation PodsDummy_Pods_AllInOneTests
-@end
--- AllInOne/Pods/Target Support Files/Pods-AllInOneUITests/Pods-AllInOneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AllInOneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AllInOneUITests
-@end
