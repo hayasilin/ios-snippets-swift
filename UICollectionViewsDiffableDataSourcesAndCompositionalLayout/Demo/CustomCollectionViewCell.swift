dbacff3b2c7247d076a8bdb9c7827378c94fb1f0
hayasilin <harvey.hayasi@gmail.com>
2023-10-31 20:34:20 +0900

    add project for UICollectionViewDiffableDataSources and Compositional layout

---
+++ UICollectionViewsDiffableDataSourcesAndCompositionalLayout/Demo/CustomCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  CustomCollectionViewCell.swift
+//  Demo
+//
+//  Created by user on 2023/07/04.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    static let cellReuseIdentifier = String(describing: CustomCollectionViewCell.self)
+
+    lazy var textLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        contentView.backgroundColor = .lightGray
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(textLabel)
+
+        NSLayoutConstraint.activate([
+            textLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+    }
+}
---
+++ UICollectionViewsDiffableDataSourcesAndCompositionalLayout/Demo/DetailViewController.swift
@@ -0,0 +1,109 @@
+//
+//  DetailViewController.swift
+//  Demo
+//
+//  Created by user on 2023/04/13.
+//
+
+import UIKit
+
+// https://velog.io/@whitehyun/iOS-UICollectionViewCompositionalLayout
+class DetailViewController: UIViewController {
+    private var sections = Section.allSections
+    private lazy var dataSource = makeDataSource()
+    private var searchController = UISearchController(searchResultsController: nil)
+
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        configureLayout()
+        applySnapshot()
+    }
+
+    private func setupViews() {
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.cellReuseIdentifier)
+        collectionView.register(SectionHeaderReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeaderReusableView.reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = makeDataSource()
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+    private func configureLayout() {
+        collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(sectionProvider: { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1/4),
+                    heightDimension: .fractionalHeight(1/3)
+                ),
+                repeatingSubitem: item,
+                count: 4
+            )
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        })
+    }
+
+    private func makeDataSource() -> DataSource {
+        let dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: CustomCollectionViewCell.cellReuseIdentifier,
+                for: indexPath) as? CustomCollectionViewCell else {
+                return .init()
+            }
+            cell.textLabel.text = itemIdentifier.title
+            return cell
+        }
+
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader else {
+                return nil
+            }
+            let section = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+            let view = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: SectionHeaderReusableView.reuseIdentifier,
+                for: indexPath) as? SectionHeaderReusableView
+            view?.titleLabel.text = section.title
+            return view
+        }
+
+        return dataSource
+    }
+
+    private func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections(sections)
+        sections.forEach { section in
+            snapshot.appendItems(section.items, toSection: section)
+        }
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+}
+
+extension DetailViewController: UICollectionViewDelegate {
+
+}
